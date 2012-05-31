80efbced4afa5df0fbaa21db7baa03529aa190b7
Shiki Okasaka <shiki.okasaka@gmail.com>
2012-05-31 12:15:01 +0000

    (readAsPng) : Switch over to png_set_expand_gray_1_2_4_to_8; png_set_gray_1_2_4_to_8 has been deprecated.
    git-svn-id: http://es-operating-system.googlecode.com/svn/trunk/www@2742 d15c3b8f-5b43-0410-be93-4d5a55ce2954

--- css/BoxImage.cpp
+++ css/BoxImage.cpp
@@ -75,7 +75,7 @@ unsigned char* readAsPng(FILE* file, unsigned& width, unsigned& height, unsigned
     if (color_type == PNG_COLOR_TYPE_PALETTE)
         png_set_palette_to_rgb(png_ptr);
     if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-        png_set_gray_1_2_4_to_8(png_ptr);
+        png_set_expand_gray_1_2_4_to_8(png_ptr);
     if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
         png_set_gray_to_rgb(png_ptr);
     if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) {
