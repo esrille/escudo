012c4c2c21d052ae8f96236467c67c627a243269
Shiki Okasaka <shiki.okasaka@gmail.com>
2012-07-12 14:36:30 +0000

    (URI::percentEncode) : Fix a bug.
    git-svn-id: http://es-operating-system.googlecode.com/svn/trunk/escort@2857 d15c3b8f-5b43-0410-be93-4d5a55ce2954

--- src/url/URI.cpp
+++ src/url/URI.cpp
@@ -37,13 +37,15 @@ std::string URI::percentEncode(const std::u16string& string, size_t pos, size_t 
         char* end = utf32to8(utf32, utf8);
         assert(end);
         for (const char* p = utf8; p < end; ++p) {
-            if (*p <= 0x20 || (*p < 127 && strchr("\"#%<>[\\]^{|}", *p))) {
+            // Note percent-encoding characters in the original URL value must be preserved.
+            if (isalnum(*p) || strchr("%" "!*'();:@&=+$,/?#[]-_.~", *p))
+                encoding += *p;
+            else {
                 encoding += '%';
                 char hex[3];
                 sprintf(hex, "%02X", *p & 0xff);
                 encoding += hex;
-            } else
-                encoding += *p;
+            }
         }
     }
     return encoding;
