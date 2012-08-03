esrille/escudo

commit 07a348e9362f2942d9f22522ebd6b93459990013

(TableWrapperBox::layOut, TableWrapperBox::layOutAuto, TableWrapperBox::layOutAutoColgroup, BlockLevelBox::layOutInlineLevelBox, BlockLevelBox::layOutInline, BlockLevelBox::layOutChildren, BlockLevelBox::layOut, FormattingContext::nextLine) : Refine automatic table layout. (BlockLevelBox::layOutText) : Compute the minimum content width of the contained box. (Box::getMCW, Box::unresolveStyle, BlockLevelBox::updateMCW, CellBox::adjustWidth, CSSAutoLengthValueImp::getPercentage, CSSStyleDeclarationImp::unresolve, StackingContext::removeBox, ) : New.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk/escort@2873 d15c3b8f-5b43-0410-be93-4d5a55ce2954

src/css/CSSStyleDeclarationImp.cpp
@@ -1365,8 +1365,7 @@ void CSSStyleDeclarationImp::inheritProperties(const CSSStyleDeclarationImp* par
 
 void CSSStyleDeclarationImp::compute(ViewCSSImp* view, CSSStyleDeclarationImp* parentStyle, Element element)
 {
-    resolved = false;   // This style needs to be resolved later.
-    clearBox();
+    unresolve();  // This style needs to be resolved later.
 
     if (this == parentStyle)
         return;

src/css/CSSStyleDeclarationImp.h
@@ -419,6 +419,10 @@ public:
     void compute(ViewCSSImp* view, CSSStyleDeclarationImp* parentStyle, Element element);
     void computeStackingContext(ViewCSSImp* view, CSSStyleDeclarationImp* parentStyle);
     void resolve(ViewCSSImp* view, const ContainingBlock* containingBlock);
+    void unresolve() {
+        resolved = false;
+        clearBox();
+    }
 
     bool resolveOffset(float& x, float &y);
 

src/css/Box.cpp
@@ -85,6 +85,9 @@ Box::Box(Node node) :
 
 Box::~Box()
 {
+    if (stackingContext)
+        stackingContext->removeBox(this);
+
     while (0 < childCount) {
         Box* child = removeChild(firstChild);
         child->release_();
@@ -160,6 +163,14 @@ void Box::restyle(ViewCSSImp* view, CSSStyleDeclarationImp* parentStyle)
         style->recompute(view, parentStyle, getNode());
     for (Box* i = firstChild; i; i = i->nextSibling)
         i->restyle(view, style);
+}
+
+void Box::unresolveStyle()
+{
+    if (CSSStyleDeclarationImp* style = getStyle())
+        style->unresolve();
+    for (Box* i = firstChild; i; i = i->nextSibling)
+        i->unresolveStyle();
 }
 
 float Box::getOutlineWidth() const
@@ -331,7 +342,8 @@ BlockLevelBox::BlockLevelBox(Node node, CSSStyleDeclarationImp* style) :
     edge(0.0f),
     remainingHeight(0.0f),
     defaultBaseline(0.0f),
-    defaultLineHeight(0.0f)
+    defaultLineHeight(0.0f),
+    mcw(0.0f)
 {
     if (style)
         setStyle(style);
@@ -527,27 +539,27 @@ void BlockLevelBox::resolveMargin(ViewCSSImp* view, const ContainingBlock* conta
         height = style->height.getPx();
 }
 
-void BlockLevelBox::layOutInlineLevelBox(ViewCSSImp* view, Node node, FormattingContext* context,
-                                         Element element, CSSStyleDeclarationImp* style)
+InlineLevelBox* BlockLevelBox::layOutInlineLevelBox(ViewCSSImp* view, Node node, FormattingContext* context,
+                                                    Element element, CSSStyleDeclarationImp* style)
 {
     assert(element);
     assert(style);
 
     if (!context->lineBox) {
         if (!context->addLineBox(view, this))
-            return;  // TODO error
+            return 0;  // TODO error
     }
 
     InlineLevelBox* inlineLevelBox = new(std::nothrow) InlineLevelBox(node, style);
     if (!inlineLevelBox)
-        return;  // TODO error
+        return 0;  // TODO error
 
     inlineLevelBox->parentBox = context->lineBox;  // for getContainingBlock
     context->prevChar = 0;
 
     BlockLevelBox* inlineBlock = view->layOutBlockBoxes(element, 0, style->parentStyle, style, true);
     if (!inlineBlock)
-        return;  // TODO error
+        return 0;  // TODO error
     inlineBlock->establishFormattingContext();
     inlineLevelBox->appendChild(inlineBlock);
     inlineBlock->layOut(view, context);
@@ -566,7 +578,7 @@ void BlockLevelBox::layOutInlineLevelBox(ViewCSSImp* view, Node node, Formatting
         if (context->lineBox->hasChildBoxes() || context->hasNewFloats()) {
             context->nextLine(view, this, false);
             if (!context->addLineBox(view, this))
-                return;  // TODO error
+                return 0;  // TODO error
             continue;
         }
         if (!context->shiftDownLineBox(view))
@@ -578,6 +590,7 @@ void BlockLevelBox::layOutInlineLevelBox(ViewCSSImp* view, Node node, Formatting
     context->appendInlineBox(view, inlineLevelBox, style);
 
     style->addBox(inlineLevelBox);
+    return inlineLevelBox;
 }
 
 void BlockLevelBox::layOutFloat(ViewCSSImp* view, Node node, BlockLevelBox* floatBox, FormattingContext* context)
@@ -714,7 +727,12 @@ bool BlockLevelBox::layOutInline(ViewCSSImp* view, FormattingContext* context, f
     marginUsed = false;
     context->atLineHead = true;
 
-    assert(!hasChildBoxes());
+    while (hasChildBoxes()) {
+        Box* child = getFirstChild();
+        removeChild(child);
+        child->release_();
+    }
+
     bool collapsed = true;
     for (auto i = inlines.begin(); i != inlines.end(); ++i) {
         Node node = *i;
@@ -744,6 +762,7 @@ bool BlockLevelBox::layOutInline(ViewCSSImp* view, FormattingContext* context, f
             } else if (style->display.isTableParts()) {
                 context->useMargin(this);
                 layOutAnonymousInlineTable(view, context, i);
+                // TODO: updateMCW
                 collapsed = false;
             } else if ((!isReplacedElement(element) && style->display.isInline()) || style->display.isListItem()) {
                 // empty inline element
@@ -752,7 +771,8 @@ bool BlockLevelBox::layOutInline(ViewCSSImp* view, FormattingContext* context, f
             } else {
                 context->useMargin(this);
                 // At this point, node is an inline block element or a replaced element.
-                layOutInlineLevelBox(view, node, context, element, style);
+                if (InlineLevelBox* box = layOutInlineLevelBox(view, node, context, element, style))
+                    updateMCW(box->getTotalWidth());
                 collapsed = false;
             }
         }
@@ -1156,8 +1176,11 @@ void BlockLevelBox::layOutChildren(ViewCSSImp* view, FormattingContext* context)
     Box* next;
     for (Box* child = getFirstChild(); child; child = next) {
         next = child->getNextSibling();
-        if (!child->layOut(view, context))
+        if (!child->layOut(view, context)) {
             removeChild(child);
+            child->release_();
+        } else
+            updateMCW(child->getMCW());
     }
 }
 
@@ -1206,7 +1229,10 @@ bool BlockLevelBox::layOut(ViewCSSImp* view, FormattingContext* context)
     if (!style)
         return false;  // TODO error
 
+    mcw = 0.0f;
     if (!isAnonymous()) {
+        if (!style->width.isAuto() && !style->width.isPercentage())
+            mcw = style->width.getPx();
         style->resolve(view, containingBlock);
         resolveWidth(view, containingBlock);
     } else {
@@ -1221,6 +1247,10 @@ bool BlockLevelBox::layOut(ViewCSSImp* view, FormattingContext* context)
         height = 0.0f;
         stackingContext = style->getStackingContext();
     }
+
+    CellBox* cell = dynamic_cast<CellBox*>(this);
+    if (cell)
+        cell->adjustWidth();
 
     visibility = style->visibility.getValue();
     textAlign = style->textAlign.getValue();
@@ -1254,13 +1284,21 @@ bool BlockLevelBox::layOut(ViewCSSImp* view, FormattingContext* context)
             !intrinsic)
             shrinkToFit();
         applyMinMaxWidth(getTotalWidth());
-    } else if (dynamic_cast<CellBox*>(this))
+
+        mcw += borderLeft + borderRight;
+        if (!style->paddingLeft.isPercentage())
+            mcw += style->paddingLeft.getPx();
+        if (!style->paddingRight.isPercentage())
+            mcw += style->paddingRight.getPx();
+        if (!style->marginLeft.isPercentage() && !style->marginLeft.isAuto())
+            mcw += style->marginLeft.getPx();
+        if (!style->marginRight.isPercentage() && !style->marginRight.isAuto())
+            mcw += style->marginRight.getPx();
+    } else if (cell)
         shrinkToFit();
 
     // Collapse margins with the first and the last children before calculating the auto height.
     collapseMarginBottom(context);
-
-    CellBox* cell = dynamic_cast<CellBox*>(this);
 
     if ((style->height.isAuto() && !intrinsic) || isAnonymous() || cell) {
         float totalClearance = 0.0f;

src/css/Box.h
@@ -20,6 +20,7 @@
 #include <Object.h>
 #include <org/w3c/dom/Text.h>
 
+#include <algorithm>
 #include <list>
 #include <string>
 
@@ -490,6 +491,7 @@ public:
     }
     void setStyle(CSSStyleDeclarationImp* style);
     void restyle(ViewCSSImp* view, CSSStyleDeclarationImp* parentStyle = 0);
+    void unresolveStyle();
 
     bool isStatic() const {
         return position == CSSPositionValueImp::Static;
@@ -546,6 +548,10 @@ public:
     virtual void resolveXY(ViewCSSImp* view, float left, float top, BlockLevelBox* clip) = 0;
     virtual bool layOut(ViewCSSImp* view, FormattingContext* context) {
         return true;
+    }
+
+    virtual float getMCW() const {
+        return 0.0f;
     }
 
     bool isVisible() const {
@@ -648,6 +654,9 @@ class BlockLevelBox : public Box
     float defaultBaseline;
     float defaultLineHeight;
 
+    // for automatic table layout
+    float mcw;
+
     void getPsuedoStyles(ViewCSSImp* view, FormattingContext* context, CSSStyleDeclarationImp* style,
                          CSSStyleDeclarationPtr& firstLetterStyle, CSSStyleDeclarationPtr& firstLineStyle);
     void nextLine(ViewCSSImp* view, FormattingContext* context, CSSStyleDeclarationImp*& activeStyle,
@@ -659,8 +668,8 @@ class BlockLevelBox : public Box
                       FontGlyph*& glyph, std::u16string& transformed);
     bool layOutText(ViewCSSImp* view, Node text, FormattingContext* context,
                     std::u16string data, Element element, CSSStyleDeclarationImp* style);
-    void layOutInlineLevelBox(ViewCSSImp* view, Node node, FormattingContext* context,
-                              Element element, CSSStyleDeclarationImp* style);
+    InlineLevelBox* layOutInlineLevelBox(ViewCSSImp* view, Node node, FormattingContext* context,
+                                         Element element, CSSStyleDeclarationImp* style);
     void layOutFloat(ViewCSSImp* view, Node node, BlockLevelBox* floatBox, FormattingContext* context);
     void layOutAbsolute(ViewCSSImp* view, Node node, BlockLevelBox* absBox, FormattingContext* context);  // 1st pass
     void layOutAnonymousInlineTable(ViewCSSImp* view, FormattingContext* context, std::list<Node>::iterator& i);
@@ -767,6 +776,10 @@ public:
     void resolveNormalWidth(float w, float r = NAN);
     void resolveFloatWidth(float w, float r = NAN);
 
+    virtual float getMCW() const {
+        return mcw;
+    }
+
     virtual void resolveOffset(float& x, float &y);
     virtual void resolveXY(ViewCSSImp* view, float left, float top, BlockLevelBox* clip);
     virtual bool layOut(ViewCSSImp* view, FormattingContext* context);
@@ -779,6 +792,10 @@ public:
     void renderInline(ViewCSSImp* view, StackingContext* stackingContext);
 
     bool isTableBox() const;
+    float updateMCW(float w) {
+        mcw = std::max(w, mcw);
+        return mcw;
+    }
 };
 
 typedef boost::intrusive_ptr<BlockLevelBox> BlockLevelBoxPtr;

src/css/CSSPropertyValueImp.h
@@ -336,6 +336,10 @@ public:
     bool isPercentage() const {
         return length.isPercentage();
     }
+    float getPercentage() const {
+        assert(isPercentage());
+        return length.number;
+    }
     virtual std::u16string getCssText(CSSStyleDeclarationImp* decl) {
         if (isAuto())
             return u"auto";

src/css/FormattingContext.cpp
@@ -365,6 +365,7 @@ void FormattingContext::nextLine(ViewCSSImp* view, BlockLevelBox* parentBox, boo
             floatBox->inserted = true;
             lineBox->insertBefore(floatBox, lineBox->getFirstChild());
             floatList.push_back(floatBox);
+            parentBox->updateMCW(floatBox->mcw);
         }
     }
     for (auto i = right.begin(); i != right.end(); ++i) {
@@ -374,8 +375,9 @@ void FormattingContext::nextLine(ViewCSSImp* view, BlockLevelBox* parentBox, boo
             // We would need a gap before the 1st right floating box to be added.
             if (!lineBox->rightBox)
                 lineBox->rightBox = floatBox;
-            lineBox->appendChild(*i);
+            lineBox->appendChild(floatBox);
             floatList.push_back(floatBox);
+            parentBox->updateMCW(floatBox->mcw);
         }
     }
     float height = lineBox->getClearance() + lineBox->getTotalHeight();

src/css/LineBox.cpp
@@ -289,12 +289,12 @@ bool BlockLevelBox::layOutText(ViewCSSImp* view, Node text, FormattingContext* c
         }
         LineBox* lineBox = context->lineBox;
 
+        float nbl = 0.0f;
         if (wrapBox) {
-            for (InlineLevelBox* box = wrapBox; box; box = dynamic_cast<InlineLevelBox*>(box->getNextSibling())) {
-                float wrapWidth = box->getTotalWidth();
-                context->x += wrapWidth;
-                context->leftover -= wrapWidth;
-            }
+            for (InlineLevelBox* box = wrapBox; box; box = dynamic_cast<InlineLevelBox*>(box->getNextSibling()))
+                nbl += box->getTotalWidth();
+            context->x += nbl;
+            context->leftover -= nbl;
             if (context->leftover < 0.0f && (lineBox->hasChildBoxes() || context->hasNewFloats())) {
                 nextLine(view, context, activeStyle, firstLetterStyle, firstLineStyle, style, false, font, point);
                 continue;
@@ -376,6 +376,7 @@ bool BlockLevelBox::layOutText(ViewCSSImp* view, Node text, FormattingContext* c
                             advanced += w;
                             context->leftover -= w;
                             wrap = length = next - position - 1;
+                            updateMCW(nbl + w);
                             goto BreakLine;
                         }
                     }
@@ -416,11 +417,11 @@ bool BlockLevelBox::layOutText(ViewCSSImp* view, Node text, FormattingContext* c
                         goto NextLine;
                     if (context->shiftDownLineBox(view)) {
                         if (wrapBox) {
-                            for (InlineLevelBox* box = wrapBox; box; box = dynamic_cast<InlineLevelBox*>(box->getNextSibling())) {
-                                float wrapWidth = box->getTotalWidth();
-                                context->x += wrapWidth;
-                                context->leftover -= wrapWidth;
-                            }
+                            nbl = 0.0f;
+                            for (InlineLevelBox* box = wrapBox; box; box = dynamic_cast<InlineLevelBox*>(box->getNextSibling()))
+                                nbl += box->getTotalWidth();
+                            context->x += nbl;
+                            context->leftover -= nbl;
                         }
                     } else {
                         inlineData += transformed;
@@ -428,13 +429,20 @@ bool BlockLevelBox::layOutText(ViewCSSImp* view, Node text, FormattingContext* c
                         context->leftover -= w;
                         length = next - position;
                         context->breakable = false;
+                        updateMCW(nbl + w);
                         goto BreakLine;
                     }
                 }
+
                 inlineData += transformed;
                 advanced += w;
                 context->leftover -= w;
                 length = next - position;
+                if (context->breakable || activeStyle->whiteSpace.isBreakingLines()) {
+                    updateMCW(nbl + w);
+                    nbl = 0.0f;
+                } else
+                    updateMCW(nbl + advanced);
                 context->breakable = false;
                 if (wrap < next && data[next - 1] == '\n') {
                     linefeed = true;

src/css/StackingContext.cpp
@@ -249,6 +249,35 @@ void StackingContext::addFloat(Box* box)
     box->nextBase = 0;
 }
 
+void StackingContext::removeBox(Box* box)
+{
+    Box* p = 0;
+    for (Box* i = firstBase; i ; p = i, i = i->nextBase) {
+        if (i == box) {
+            if (!p)
+                firstBase = i->nextBase;
+            else
+                p->nextBase = i->nextBase;
+            if (lastBase == box)
+                lastBase = p;
+            break;
+        }
+    }
+
+    p = 0;
+    for (Box* i = firstRenderBase; i ; p = i, i = i->nextBase) {
+        if (i == box) {
+            if (!p)
+                firstRenderBase = i->nextBase;
+            else
+                p->nextBase = i->nextBase;
+            if (lastRenderBase == box)
+                lastRenderBase = p;
+            break;
+        }
+    }
+}
+
 void StackingContext::dump(std::string indent)
 {
     std::cout << indent << "z-index: " << zIndex << '\n';

commit 00d0ce829ef1c6a2f0d4e1d6c4a9e145b53af311

(CSSStyleDeclarationImp::unresolve, CSSStyleDeclarationImp::compute) : Fix bugs introduced in r2873.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk/escort@2893 d15c3b8f-5b43-0410-be93-4d5a55ce2954

src/css/CSSStyleDeclarationImp.cpp
@@ -1366,6 +1366,7 @@ void CSSStyleDeclarationImp::inheritProperties(const CSSStyleDeclarationImp* par
 void CSSStyleDeclarationImp::compute(ViewCSSImp* view, CSSStyleDeclarationImp* parentStyle, Element element)
 {
     unresolve();  // This style needs to be resolved later.
+    clearBox();
 
     if (this == parentStyle)
         return;

src/css/CSSStyleDeclarationImp.h
@@ -422,7 +422,6 @@ public:
     void resolve(ViewCSSImp* view, const ContainingBlock* containingBlock);
     void unresolve() {
         resolved = false;
-        clearBox();
     }
 
     bool resolveOffset(float& x, float &y);