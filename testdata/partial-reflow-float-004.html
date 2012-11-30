esrille/escudo

commit 7987675f7090a1b9b94bf6234236ac65535769e7

(Block::layOutInlineBlocks) : Fix a bug partial-reflow-float-003.html : New
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk/escort@3217 d15c3b8f-5b43-0410-be93-4d5a55ce2954

src/css/Box.cpp
@@ -1296,8 +1296,14 @@ void Block::layOutInlineBlocks(ViewCSSImp* view)
                 if (block->style->marginRight.isAuto())
                     block->marginRight = 0.0f;
             }
-            if (savedWidth != block->getTotalWidth() || savedHeight != block->getTotalHeight())
+            if (savedWidth != block->getTotalWidth() || savedHeight != block->getTotalHeight()) {
                 flags |= NEED_REFLOW;
+                // Set NEED_REFLOW to anonymous boxes between 'block' and 'this'.
+                for (Box* ancestor = block->parentBox; ancestor && ancestor != this; ancestor = ancestor->parentBox) {
+                    if (dynamic_cast<Block*>(ancestor))
+                        ancestor->flags |= NEED_REFLOW;
+                }
+            }
         }
     }
 }
@@ -1385,7 +1391,7 @@ bool Block::layOut(ViewCSSImp* view, FormattingContext* context)
     context = updateFormattingContext(context);
 
     layOutInlineBlocks(view);
-    if (hasInline() && !(flags & NEED_REFLOW)) {
+    if (hasInline() && !(flags & NEED_REFLOW) && !isAnonymous()) {
         // Only inline blocks have been marked NEED_REFLOW and no more reflow is necessary
         // with this block.
 #ifndef NDEBUG

testdata/partial-reflow-float-003.html
@@ -1,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Partial reflow test</title>
+<style type="text/css">
+#t {
+  display: inline-block;
+  width: 150px;
+  height: 0;
+  background-color: green;
+}
+</style>
+<script type="text/javascript">
+function doTest() {
+  this.addEventListener('keydown', function(event) {
+    if (event.keyCode == 13) {
+      var t = document.getElementById('t');
+      t.style.height = '150px';
+    }
+  }, false);
+}
+</script>
+</head>
+<body onload="doTest()">
+<p>Press [Enter] to enlarge the green floating box.</p>
+<div style='color: green; float: right; text-align: center;'>
+  <div id='t'></div><br>
+  Filler Text<br>
+  Filler Text
+</div>
+<p>
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text 
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text
+</p>
+</body>
+</html>

commit 44871685ae5f40fc78138ab39052e05eee614676

(Block::layOut) : Fix a bug introduced in r3217 partial-reflow-float-004.html : New
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk/escort@3221 d15c3b8f-5b43-0410-be93-4d5a55ce2954

src/css/Box.cpp
@@ -1391,7 +1391,7 @@ bool Block::layOut(ViewCSSImp* view, FormattingContext* context)
     context = updateFormattingContext(context);
 
     layOutInlineBlocks(view);
-    if (hasInline() && !(flags & NEED_REFLOW) && !isAnonymous()) {
+    if (hasInline() && !(flags & NEED_REFLOW)) {
         // Only inline blocks have been marked NEED_REFLOW and no more reflow is necessary
         // with this block.
 #ifndef NDEBUG

testdata/partial-reflow-float-004.html
@@ -1,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Partial reflow test</title>
+<style type="text/css">
+#box {
+  float: right;
+  width: 150px;
+  height: 150px;
+  background-color: green;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
+<script type="text/javascript">
+function doTest() {
+  this.addEventListener('keydown', function(event) {
+    if (event.keyCode == 13) {
+      var t = document.getElementById('t');
+      t.style.textDecoration = 'underline';
+    }
+  }, false);
+}
+</script>
+</head>
+<body onload="doTest()" style='margin-left: 140px;'>
+
+<p>Press [Enter] to draw an underline under the text in the silver box.</p>
+
+<div style='background-color: silver; position: absolute; left: 0px; top: 0px; width: 130px; height: 100%;'>
+  <span id='t'>Filler Text</span>
+</div>
+
+<div id='box'>
+</div>
+
+<p>
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text
+Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text Filler Text
+</p>
+</body>
+</html>