esrille/escudo

commit 5de0982c44ddd7d88eb5556dff64b1b2afb6861b

[CSSOM View] document.elementFromPoint() should return the top-most element
(StackingContext::boxFromPoint): New
(ViewCSSImp::boxFromPoint, Box::boxFromPoint): Refine

src/css/Box.h
@@ -447,18 +447,22 @@ public:
         return v < b;
     }
 
-    Box* boxFromPoint(int x, int y) {
-        // TODO: Check z-index and relative offset
-        if (!isAnonymous() && Element::hasInstance(node)) {
-            Element element = interface_cast<Element>(node);
-            x += element.getScrollLeft();
-            y += element.getScrollTop();
+    Box* boxFromPoint(int x, int y, StackingContext* context = 0) {
+        if (context && stackingContext != context)
+            return 0;
+        if (isInside(x, y)) {
+            if (!isAnonymous() && Element::hasInstance(node)) {
+                Element element = interface_cast<Element>(node);
+                x += element.getScrollLeft();
+                y += element.getScrollTop();
+            }
+            for (Box* box = getFirstChild(); box; box = box->getNextSibling()) {
+                if (Box* target = box->boxFromPoint(x, y, context))
+                    return target;
+            }
+            return this;
         }
-        for (Box* box = getFirstChild(); box; box = box->getNextSibling()) {
-            if (Box* target = box->boxFromPoint(x, y))
-                return target;
-        }
-        return isInside(x, y) ? this : 0;
+        return 0;
     }
 
     void updateScrollSize();

src/css/StackingContext.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011, 2012 Esrille Inc.
+ * Copyright 2011-2013 Esrille Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -416,6 +416,28 @@ void StackingContext::layOutAbsolute(ViewCSSImp* view)
             }
         }
     } while (repeat);
+}
+
+Box* StackingContext::boxFromPoint(int x, int y)
+{
+    StackingContext* childContext;
+    for (childContext = getLastChild(); childContext && 0 <= childContext->zIndex; childContext = childContext->getPreviousSibling()) {
+        if (Box* target = childContext->boxFromPoint(x, y))
+            return target;
+    }
+    if (firstBase) {
+        int s = x - relativeX;
+        int t = y - relativeY;
+        for (Box* base = firstBase; base; base = base->nextBase) {
+            if (Box* target = base->boxFromPoint(s, t, this))
+                return target;
+        }
+    }
+    for (; childContext; childContext = childContext->getPreviousSibling()) {
+        if (Box* target = childContext->boxFromPoint(x, y))
+            return target;
+    }
+    return 0;
 }
 
 void StackingContext::dump(std::string indent)

src/css/StackingContext.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011, 2012 Esrille Inc.
+ * Copyright 2011-2013 Esrille Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -166,6 +166,8 @@ public:
     float getRelativeY() const {
         return relativeY;
     }
+
+    Box* boxFromPoint(int x, int y);
 
     void dump(std::string indent = "");
 };

src/css/ViewCSSImp.cpp
@@ -103,12 +103,12 @@ ViewCSSImp::~ViewCSSImp()
 
 Box* ViewCSSImp::boxFromPoint(int x, int y)
 {
-    if (!boxTree)
-        return 0;
-    x += window->getScrollX();
-    y += window->getScrollY();
-    if (Box* target = boxTree.get()->boxFromPoint(x, y))
-        return target;
+    if (stackingContexts) {
+        x += window->getScrollX();
+        y += window->getScrollY();
+        if (Box* target = stackingContexts->boxFromPoint(x, y))
+            return target;
+    }
     return boxTree.get();
 }
 

commit a8e4211d5b02528ee22cac52c42396bfba247b04

(Box::boxFromPoint): Fix a regression since 5de0982c44

src/css/Box.cpp
@@ -69,13 +69,13 @@ Box::Box(Node node) :
     position(CSSPositionValueImp::Static),
     offsetH(0.0f),
     offsetV(0.0f),
+    visibility(CSSVisibilityValueImp::Visible),
     stackingContext(0),
     nextBase(0),
     nextFloat(0),
     intrinsic(false),
     x(0.0f),
     y(0.0f),
-    visibility(CSSVisibilityValueImp::Visible),
     clipBox(0),
     backgroundColor(0x00000000),
     backgroundRequest(0),

src/css/Box.h
@@ -124,6 +124,8 @@ protected:
     float offsetH;
     float offsetV;
 
+    unsigned visibility;
+
     StackingContextPtr stackingContext;
     Box* nextBase;
     Box* nextFloat;
@@ -132,8 +134,6 @@ protected:
 
     float x;  // in screen coord
     float y;  // in screen coord
-
-    unsigned visibility;
 
     Block* clipBox;
 
@@ -447,21 +447,18 @@ public:
     }
 
     Box* boxFromPoint(int x, int y, StackingContext* context = 0) {
-        if (context && stackingContext != context)
+        if (context && stackingContext && stackingContext != context)
             return 0;
-        if (isInside(x, y)) {
-            if (!isAnonymous() && Element::hasInstance(node)) {
-                Element element = interface_cast<Element>(node);
-                x += element.getScrollLeft();
-                y += element.getScrollTop();
-            }
-            for (Box* box = getFirstChild(); box; box = box->getNextSibling()) {
-                if (Box* target = box->boxFromPoint(x, y, context))
-                    return target;
-            }
-            return this;
+        if (!isAnonymous() && Element::hasInstance(node)) {
+            Element element = interface_cast<Element>(node);
+            x += element.getScrollLeft();
+            y += element.getScrollTop();
         }
-        return 0;
+        for (Box* box = getFirstChild(); box; box = box->getNextSibling()) {
+            if (Box* target = box->boxFromPoint(x, y, context))
+                return target;
+        }
+        return isInside(x, y) ? this : 0;
     }
 
     void updateScrollSize();