esrille/escudo

commit 04cd71924c1ee07df281c58d03eca84c88b946f9

[CSS] Support stylesheets for non-CSS presentational hints
(ViewCSSImp::updateStyleRules): Check non-CSS presentational hints from the presentational hints stylesheet
(ViewCSSImp::ViewCSSImp): Get stylesheets from DOMImplementationImp; all callers changed.
(CSSRuleListImp::Importance::Presentational): New
(CSSRuleListImp::PrioritizedRule::PrioritizedRule): Set specificity of presentational hints to zero
(HTMLElementImp::evalNoWrap): Remove; nowrap attribute is handled by preshint.css
(HTMLTableCellElementImp::eval): Refine
data/escort/preshint.css: New
testdata/preshint.css: New
escort.spec.in: Update %files for escort/preshint.css
src/Script.test.cpp: Support the --preshint=<path> option
src/Navigator.test.cpp: Load presentational hints from profile
src/Escort.cpp: Load presentational hints from profile

src/css/ViewCSSImp.cpp
@@ -33,6 +33,7 @@
 #include "CSSStyleDeclarationImp.h"
 #include "CSSStyleSheetImp.h"
 #include "DocumentImp.h"
+#include "DOMImplementationImp.h"
 #include "MediaListImp.h"
 #include "html/HTMLElementImp.h"
 #include "html/HTMLTemplateElementImp.h"
@@ -64,13 +65,11 @@ Block* getCurrentBox(CSSStyleDeclarationImp* style, bool asTablePart)
 
 }
 
-ViewCSSImp::ViewCSSImp(DocumentWindowPtr window, css::CSSStyleSheet defaultStyleSheet, css::CSSStyleSheet userStyleSheet) :
+ViewCSSImp::ViewCSSImp(DocumentWindowPtr window) :
     window(window),
     dpi(96),
     zoom(1.0f),
     mutationListener(boost::bind(&ViewCSSImp::handleMutation, this, _1, _2)),
-    defaultStyleSheet(defaultStyleSheet),
-    userStyleSheet(userStyleSheet),
     overflow(CSSOverflowValueImp::Auto),
     stackingContexts(0),
     hovered(0),
@@ -227,24 +226,23 @@ Element ViewCSSImp::updateStyleRules(Element element, CSSStyleDeclarationImp* st
         elementDecl = dynamic_cast<CSSStyleDeclarationImp*>(htmlElement.getStyle().self());
     }
 
-    if (CSSStyleSheetImp* sheet = dynamic_cast<CSSStyleSheetImp*>(defaultStyleSheet.self()))
+    if (CSSStyleSheetImp* sheet = getDOMImplementation()->getDefaultStyleSheet())
         findDeclarations(style->ruleSet, element, sheet->getCssRules(), CSSRuleListImp::UserAgent);
-    if (CSSStyleSheetImp* sheet = dynamic_cast<CSSStyleSheetImp*>(userStyleSheet.self()))
+    if (CSSStyleSheetImp* sheet = getDOMImplementation()->getUserStyleSheet())
         findDeclarations(style->ruleSet, element, sheet->getCssRules(), CSSRuleListImp::User);
     if (elementDecl) {
-        CSSStyleDeclarationImp* nonCSS = elementDecl->getPseudoElementStyle(CSSPseudoElementSelector::NonCSS);
-        if (nonCSS) {
+        if (CSSStyleDeclarationImp* nonCSS = elementDecl->getPseudoElementStyle(CSSPseudoElementSelector::NonCSS)) {
             // TODO: emplace() seems to be not ready yet with libstdc++.
-            CSSRuleListImp::PrioritizedRule rule(CSSRuleListImp::User, nonCSS);
+            CSSRuleListImp::PrioritizedRule rule(CSSRuleListImp::Presentational, nonCSS);
             style->ruleSet.insert(rule);
         }
     }
-    unsigned importance = CSSRuleListImp::Author;
+    if (CSSStyleSheetImp* sheet = getDOMImplementation()->getPresentationalHints())
+        findDeclarations(style->ruleSet, element, sheet->getCssRules(), CSSRuleListImp::Presentational);
     stylesheets::StyleSheetList styleSheetList(getDocument().getStyleSheets());
     for (unsigned i = 0; i < styleSheetList.getLength(); ++i) {
         CSSStyleSheetImp* sheet = dynamic_cast<CSSStyleSheetImp*>(styleSheetList.getElement(i).self());
-        findDeclarations(style->ruleSet, element, sheet->getCssRules(), importance++);
-        // TODO: Check overflow of importance
+        findDeclarations(style->ruleSet, element, sheet->getCssRules(), CSSRuleListImp::Author);
     }
 
     style->compute(this, parentStyle, element);

Makefile.am
@@ -1316,7 +1316,8 @@ dist_escortdata_DATA =  \
 	data/escort/go-next.png \
 	data/escort/go-previous.png \
 	data/escort/view-refresh.png \
-	data/escort/process-stop.png
+	data/escort/process-stop.png \
+	data/escort/preshint.css
 
 dist_aboutdata_DATA =  \
 	data/escort/about/blank \

Makefile.in
@@ -1713,7 +1713,8 @@ dist_escortdata_DATA = \
 	data/escort/go-next.png \
 	data/escort/go-previous.png \
 	data/escort/view-refresh.png \
-	data/escort/process-stop.png
+	data/escort/process-stop.png \
+	data/escort/preshint.css
 
 dist_aboutdata_DATA = \
 	data/escort/about/blank \

data/escort/preshint.css
@@ -1,0 +1,3 @@
+/* cf. http://www.w3.org/TR/html5/rendering.html#the-css-user-agent-style-sheet-and-presentational-hints */
+
+td[nowrap], th[nowrap] { white-space: nowrap; }

escort.spec.in
@@ -71,6 +71,7 @@ gtk-update-icon-cache %{_datadir}/icons/hicolor &>/dev/null || :
 %{_datadir}/esrille/escort/go-previous.png
 %{_datadir}/esrille/escort/view-refresh.png
 %{_datadir}/esrille/escort/process-stop.png
+%{_datadir}/esrille/escort/preshint.css
 %dir %{_datadir}/esrille/escort/about
 %{_datadir}/esrille/escort/about/blank
 %{_datadir}/esrille/escort/about/index.html

src/BackgroundTask.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Esrille Inc.
+ * Copyright 2012, 2013 Esrille Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,7 +90,7 @@ void WindowImp::BackgroundTask::operator()()
             state = Cascading;
             recordTime("%*sselector matching begin", window->windowDepth * 2, "");
             if (!view)
-                view = new(std::nothrow) ViewCSSImp(window->getDocumentWindow(), getDOMImplementation()->getDefaultCSSStyleSheet(), getDOMImplementation()->getUserCSSStyleSheet());
+                view = new(std::nothrow) ViewCSSImp(window->getDocumentWindow());
             if (view) {
                 view->constructComputedStyles();
                 state = Cascaded;

src/Box.test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2012 Esrille Inc.
+ * Copyright 2010-2013 Esrille Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -87,7 +87,7 @@ int main(int argc, char** argv)
             return EXIT_FAILURE;
         }
         defaultStyleSheet = loadStyleSheet(stream);
-        getDOMImplementation()->setDefaultCSSStyleSheet(defaultStyleSheet);
+        getDOMImplementation()->setDefaultStyleSheet(defaultStyleSheet);
     }
 
     document = loadDocument(htmlDocument);
@@ -102,7 +102,7 @@ int main(int argc, char** argv)
     // create the default view
     DocumentWindowPtr window = new(std::nothrow) DocumentWindow;
     window->setDocument(document);
-    ViewCSSImp* view = new ViewCSSImp(window, defaultStyleSheet);
+    ViewCSSImp* view = new ViewCSSImp(window);
     view->setSize(8.5f * 96, 11.0f * 96);  // US letter size, 96 DPI
     view->constructComputedStyles();
     view->calculateComputedStyles();

src/CSSStyle.test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2012 Esrille Inc.
+ * Copyright 2010-2013 Esrille Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -110,7 +110,7 @@ int main(int argc, char** argv)
             return EXIT_FAILURE;
         }
         defaultStyleSheet = loadStyleSheet(stream);
-        getDOMImplementation()->setDefaultCSSStyleSheet(defaultStyleSheet);
+        getDOMImplementation()->setDefaultStyleSheet(defaultStyleSheet);
     }
 
     document = loadDocument(htmlDocument);
@@ -123,7 +123,7 @@ int main(int argc, char** argv)
     // create the default view
     DocumentWindowPtr window = new(std::nothrow) DocumentWindow;
     window->setDocument(document);
-    ViewCSSImp* view = new ViewCSSImp(window, defaultStyleSheet);
+    ViewCSSImp* view = new ViewCSSImp(window);
     view->constructComputedStyles();
     view->calculateComputedStyles();
     printComputedValues(document, view);

commit 2a724e93602566ae3fb0645bc27001662cba8097

(ViewCSSImp::updateStyleRules): Fix a regression in 04cd71924c1ee07df281c58d03eca84c88b946f9

src/css/ViewCSSImp.cpp
@@ -239,10 +239,13 @@ Element ViewCSSImp::updateStyleRules(Element element, CSSStyleDeclarationImp* st
     }
     if (CSSStyleSheetImp* sheet = getDOMImplementation()->getPresentationalHints())
         findDeclarations(style->ruleSet, element, sheet->getCssRules(), CSSRuleListImp::Presentational);
+
+    unsigned importance = CSSRuleListImp::Author;
     stylesheets::StyleSheetList styleSheetList(getDocument().getStyleSheets());
     for (unsigned i = 0; i < styleSheetList.getLength(); ++i) {
         CSSStyleSheetImp* sheet = dynamic_cast<CSSStyleSheetImp*>(styleSheetList.getElement(i).self());
-        findDeclarations(style->ruleSet, element, sheet->getCssRules(), CSSRuleListImp::Author);
+        findDeclarations(style->ruleSet, element, sheet->getCssRules(), importance++);
+        // TODO: Check overflow of importance
     }
 
     style->compute(this, parentStyle, element);